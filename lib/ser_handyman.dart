7750dd585ce4515669430df74c20bce0cea19840
shananthika13 <shananthika13@gmail.com>
2024-09-22 21:14:19 +0530

    added

---
+++ lib/ser_handyman.dart
@@ -0,0 +1,32 @@
+mport 'package:flutter/material.dart';
+
+class ServHandyman extends StatelessWidget {
+  const ServHandyman({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'FixNow!',
+          style: TextStyle(
+            fontWeight: FontWeight.w300,
+            color: Color(0xFFEBF4F6), // Text color EBF4F6
+            fontSize: 50.0,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color(0xFF071952), // Background color #071952
+        toolbarHeight: 100, // Blue filled space with the FixNow! text
+      ),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            color: const Color(0xFFEBF4F6), // Set background color to EBF4F6
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                // "Service" Button with teal background and white text
+                ElevatedButton(
