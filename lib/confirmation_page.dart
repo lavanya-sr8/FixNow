2f6564ca484c5fd26673be2b61f7d1ad479ee8a4
shananthika13 <shananthika13@gmail.com>
2024-09-23 16:36:31 +0530

    deleted

--- lib/confirmation_page.dart
+++
@@ -1,5 +0,0 @@
-// import 'package:flutter/material.dart';
-
-// class ConfirmationPage extends StatelessWidget{
-//   const ConfirmationPage({super.key});
-// }
--- lib/rejection_page.dart
+++
@@ -1,7 +0,0 @@
-// import 'package:flutter/material.dart';
-
-// class RejectionPage extends StatelessWidget{
-//   const RejectionPage({super.key});
-
-
-// }
